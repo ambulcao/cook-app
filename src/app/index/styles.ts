cc3aecd799c6cea8af33d7b7b9ac2b1cce87c189
Alexandre Bulcao <ambulcao@gmail.com>
2024-03-06 17:28:02 +0000

    Padronizando as fontes
    Padronizando as fontes no app

--- src/app/index/styles.ts
+++ src/app/index/styles.ts
@@ -1,15 +1,21 @@
 import { StyleSheet } from "react-native"
+import { theme } from "../../theme"
 
 
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 24,
   },
   title: {
-
+    fontSize: theme.fonts.size.heading.xl,
+    lineHeight: 44,
+    marginTop: 42,
+    fontFamily: theme.fonts.family.bold,
   },
   subtitle: {
-    
+    fontSize: theme.fonts.size.body.md,
+    fontFamily: theme.fonts.family.regular,
   }
 })
