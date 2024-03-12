ff7335b483ba88dae1a32f708df984c0f81fcb46
Alexandre Bulcao <ambulcao@gmail.com>
2024-03-12 17:09:09 +0000

    Adicionado o FlatList
    Adicionado o FlatList para visualizar imagem

--- src/app/recipes/index.tsx
+++ src/app/recipes/index.tsx
@@ -1,7 +1,10 @@
 import { MaterialIcons } from "@expo/vector-icons";
-import { View } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import { styles } from "./styles";
 import { router } from "expo-router";
+import { Recipe } from "@/src/components/Recipe";
+
+
 
 export default function Recipes(){
   return (
@@ -12,7 +15,24 @@ export default function Recipes(){
           size={32}
           onPress={() => router.back()}
         />
+        <Text style={styles.title}>Ingredientes</Text>
+      
+        
       </View>
+
+      
+
+      <FlatList 
+          data={["1"]}
+          keyExtractor={(item) => item}
+          renderItem={() => (
+            <Recipe recipe={{ 
+              name: "Omelete", 
+              image: "https://img.cybercook.com.br/receitas/105/omelete-classica-1-840x480.jpeg?q=75",
+              minutes: 10,
+            }} />
+          )}
+        />
     </View>
   )
 }
